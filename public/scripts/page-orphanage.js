0df5d8f36e70abca27e32db02b7089e02e4dfc4d
Cristhinny-Sarah <cristhinnysarah@gmail.com>
2020-10-14 17:52:59 -0300

    Aula do dia 2 completa

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,47 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([-16.7167231,-49.277035], 15);
+
+//create and add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: './public/images/map-marker.svg',
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L.marker([-16.7167231,-49.277035], { icon }).addTo(map);
+
+
+/* image galery */
+function selectImage(event) {
+    const button = event.currentTarget
+
+    console.log(button.children)
+
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    imageContainer.src = image.src
+
+    button.classList.add('active')
+}
